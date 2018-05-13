1d5ebe5f7a91a9919eff9c7886379f241939abc3
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-13 03:43:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,4 +69,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if ( arguments.length == 0 ){
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    return `Your total cost is $${total}, which will be charged to the card $(cardNumber).`
+  }
 }
