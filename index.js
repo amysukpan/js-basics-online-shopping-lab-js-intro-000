5ab48f324c24e25d54834e5617eb18556d87bec6
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-13 03:52:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
     var totalprice=total()
-    
+    cart.length=0
     return `Your total cost is $${totalprice}, which will be charged to the card ${cardNumber}.`
     }
 }
